04ec189d64fa67fc8bf0d4ca1ff59bd6daddf4e4
csbrandt <cs.brandt@gmail.com>
2015-06-20 15:21:48 -0700

    Updated intersectLineBBox to return flat array of intersection points

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ WGS84IntersectUtil.intersectLineBBox = function(line, bbox) {
       intersection = geojsonUtils.lineStringsIntersect(segments[c].geometry, line.geometry);
 
       if (intersection) {
-         intersectionPoints.push(intersection);
+         Array.prototype.push.apply(intersectionPoints, intersection);
       }
    }
 
